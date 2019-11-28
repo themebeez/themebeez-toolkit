002d82039c8d13cea34a6b41ec5e04616a8e5a48
Anuj <stoneera111@gmail.com>
2019-11-28 17:10:42 +0545

    Update themebeez-toolkit-public.js

--- public/js/themebeez-toolkit-public.js
+++ public/js/themebeez-toolkit-public.js
@@ -6,11 +6,11 @@
 
         // Count & add class in Mega Menu Orchid store Theme
 
-        var megaSubMenu = $('.menu-item-has-mega-children > ul');
+        var megaSubMenu = $('.masterheader .menu-item-has-mega-children > ul');
 
         megaSubMenu.addClass('mega-menu-sub-menu');
 
-        var MegaMenuColumnCount = $(".menu-item-has-mega-children > ul.mega-menu-sub-menu > li").length;
+        var MegaMenuColumnCount = $(".masterheader .menu-item-has-mega-children > ul.mega-menu-sub-menu > li").length;
 
         megaSubMenu.addClass('mega-menu-column-'+MegaMenuColumnCount);
 
