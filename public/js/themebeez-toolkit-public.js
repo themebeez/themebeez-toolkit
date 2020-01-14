ad2510f427f44295f438dad5f67141a4166e22d1
Anuj Subedi <struggleage@gmail.com>
2020-01-14 18:28:11 +0545

    = 1.1.9 - January 15, 2019 =
    - Removed: Orchid store mega menu js
    - Removed: Public css & js files

--- public/js/themebeez-toolkit-public.js
+++ public/js/themebeez-toolkit-public.js
@@ -3,18 +3,6 @@
     'use strict';
 
     $(document).ready(function() {
-
-        // Count & add class in Mega Menu Orchid store Theme
-
-        var megaSubMenu = $('.masterheader .menu-item-has-mega-children > ul');
-
-        megaSubMenu.addClass('mega-menu-sub-menu');
-
-        var MegaMenuColumnCount = $(".masterheader .menu-item-has-mega-children > ul.mega-menu-sub-menu > li").length;
-
-        megaSubMenu.addClass('mega-menu-column-'+MegaMenuColumnCount);
-
-
     });
 
 })(jQuery);
